2f6f74e9b0d5ef33869a3ff0a4bbfc0f74a20d49
Julien DELPUECH <jdelpuec@e1r9p18.42.fr>
2020-01-24 17:08:15 +0100

    check player angle fix rotation 360C

--- sources/handle_keyboard_event.c
+++ sources/handle_keyboard_event.c
@@ -6,7 +6,7 @@
 /*   By: jdelpuec <jdelpuec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 11:15:57 by ebonafi           #+#    #+#             */
-/*   Updated: 2020/01/22 16:22:23 by jdelpuec         ###   ########.fr       */
+/*   Updated: 2020/01/24 16:34:57 by jdelpuec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int	handle_keyboard_misc(t_win *w, t_keyboard *k)
 
 float		check_line_point(t_vector_2d l1, t_vector_2d l2, t_vector_2d p)
 {
-	printf("dist_wall = %f \n", (l2.x - l1.x) * (p.y - l1.y) - (l2.y - l1.y) * (p.x - l1.x));
+	// printf("dist_wall = %f \n", (l2.x - l1.x) * (p.y - l1.y) - (l2.y - l1.y) * (p.x - l1.x));
 	return ((l2.x - l1.x) * (p.y - l1.y) - (l2.y - l1.y) * (p.x - l1.x));
 }
 
@@ -102,7 +102,7 @@ void	handle_keyboard_mvt(t_win *w, t_ray *r, t_keyboard *k)
 	ms	= (1.0 / w->fps); // temp value to be limited
 	i	= 0;
 
-	// printf("%f \n", ms);
+	printf("%f \n", r->player.angle);
 	if (k->state[SDL_SCANCODE_R] == 1)
 	{
 		r->player.position.x		= 0.0;
@@ -123,12 +123,14 @@ void	handle_keyboard_mvt(t_win *w, t_ray *r, t_keyboard *k)
 		r->player.velocity.y = -sin(r->player.angle) * 10.0;
 	}
 	if (k->state[SDL_SCANCODE_A] == 1)
-		r->player.angle -= 1.6f * ms;
+		r->player.angle -= 0.015;
 	if (k->state[SDL_SCANCODE_D] == 1)
-		r->player.angle += 1.6f * ms;
+		r->player.angle += 0.015;
 	if (k->state[SDL_SCANCODE_SPACE] == 1)
 		r->player.velocity.z = 60.0;
 
+	if (fabsf(r->player.angle) >= 6.299992)
+		r->player.angle = 0;
 	if (ms > 0.1)
 	{
 		r->player.velocity.x = 0;
@@ -136,27 +138,27 @@ void	handle_keyboard_mvt(t_win *w, t_ray *r, t_keyboard *k)
 	}
 	if (r->player.velocity.x > 1.0 || r->player.velocity.x < 1.0 || r->player.velocity.y > 1.0 || r->player.velocity.y < 1.0)
 	{
-    	r->player.velocity.y -= r->player.velocity.y * 6.0f * ms;
-		r->player.velocity.x -= r->player.velocity.x * 6.0f * ms;
 		while (i < r->sectors[r->player.sector].wall_count)
 		{
 			new_pos = (t_vector_2d) {r->player.position.x + r->player.velocity.x * ms,
 						 r->player.position.y + r->player.velocity.y * ms};
 			wall	= r->sectors[r->player.sector].walls[i];
-				if (check_line_point(wall.p1, wall.p2, new_pos) > -1.5)
+			if (check_line_point(wall.p1, wall.p2, new_pos) > -1.5)
+			{
+				if (wall.portal_sector >= 0)
 				{
-					if (wall.portal_sector >= 0)
-					{
-						r->player.sector		= wall.portal_sector;
-						break;
-					}
-					else
-					{
-						new_pos = (t_vector_2d) {wall.p2.x - wall.p1.x, wall.p2.y - wall.p1.y};
-						r->player.velocity.x = new_pos.x * (r->player.velocity.x * ms * new_pos.x + r->player.velocity.y * ms * new_pos.y) /  (new_pos.x * new_pos.x + new_pos.y + new_pos.y);
-						r->player.velocity.x = new_pos.y * (r->player.velocity.x * ms * new_pos.x + r->player.velocity.y * ms * new_pos.y) / (new_pos.x * new_pos.x + new_pos.y + new_pos.y);
-					}
+					r->player.sector		= wall.portal_sector; 								//vecteur direction, se deplacer juste apres le portail;
+					break;
 				}
+				else
+				{
+					new_pos = (t_vector_2d) {wall.p2.x - wall.p1.x, wall.p2.y - wall.p1.y};
+					r->player.velocity.x = new_pos.x * (r->player.velocity.x * ms * new_pos.x + r->player.velocity.y * ms * new_pos.y) / (new_pos.x * new_pos.x + new_pos.y + new_pos.y);
+					r->player.velocity.x = new_pos.y * (r->player.velocity.x * ms * new_pos.x + r->player.velocity.y * ms * new_pos.y) / (new_pos.x * new_pos.x + new_pos.y + new_pos.y);
+					// r->player.velocity.y = new_pos.x * (r->player.velocity.x * ms * new_pos.x + r->player.velocity.y * ms * new_pos.y) / (new_pos.x * new_pos.x + new_pos.y + new_pos.y);
+					// r->player.velocity.y = new_pos.y * (r->player.velocity.x * ms * new_pos.x + r->player.velocity.y * ms * new_pos.y) / (new_pos.x * new_pos.x + new_pos.y + new_pos.y);
+				}
+			}
 			i++;
 		}
 		r->player.position.x += r->player.velocity.x * ms;
