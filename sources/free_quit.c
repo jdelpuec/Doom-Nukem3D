70ca426e27e0d78490fa4abf2ddc621c0688ac24
jdelpuec <jdelpuec@student.42.fr>
2020-04-13 18:47:31 +0200

    leaks cleaning

--- sources/free_quit.c
+++ sources/free_quit.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 16:50:17 by user42            #+#    #+#             */
-/*   Updated: 2020/04/13 17:56:51 by user42           ###   ########.fr       */
+/*   Updated: 2020/04/13 18:47:08 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,25 @@
 
 void    free_quit( t_win *w, t_ray *r)
 {
-    t_text_tab *tmp;
-    t_text_tab *tmp2;
+    int         x;
+    t_text_tab  *tmp;
+    t_text_tab  *tmp2;
 
+    x = 0;
     tmp = w->text_list.next;
     free(w->text_list.data);
-    free(w->text_list.path);
-    free(w->text_list.prev);   
-	free(r->sectors);
+    free(w->text_list.prev);
     while(tmp->next != NULL)
     {
         tmp2 = tmp;
         tmp = tmp->next;
         free(tmp2->data);
-        // free(tmp2->path);
         free(tmp2);
     }
-    free(w->text_list.data);
+    free(tmp->data);
+    free(tmp);
+    while (x < r->sector_count)
+    	free(r->sectors[x++].walls);
+	free(r->sectors);
     free(r->check_sprt);
 }
